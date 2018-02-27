d003ce0a36f71c171873abc33981895c57469058
BradGamez <33333609+BradGamez@users.noreply.github.com>
2018-02-27 18:07:36 -0500

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -306,15 +306,14 @@ bot.on('message', message => {
 
 var cleverbot = require("cleverbot.io");
 var talkbot = new cleverbot(process.env.USERAPI,process.env.KEYAPI);
-                                        
+                                        talkbot.setNick("Abuse")
                                         bot.on('message', message => {
                                             if(message.channel.name === "abusecleverbot" || message.channel.id === "410577975616995340") {
                                             if(message.author.bot) return;
-					bot.setNick("Abuse")
-					bot.create(function (err, session) {
+					talkbot.create(function (err, session) {
                                          talkbot.ask(message.content, function (err, response) {
-					 });  
 					message.channel.send(response);
+					});
                                           });
 					    }
                                         });
