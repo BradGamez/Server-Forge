6655604f922983e81aebe5a82c1978c8ae7be32f
BradGamez <33333609+BradGamez@users.noreply.github.com>
2018-03-19 01:54:23 -0400

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -92,9 +92,6 @@ bot.on("message", message => {
       const code = args.join(" ");
       let evaled = eval(code);
 
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-
       message.channel.send(clean(evaled), {code:"xl"});
     } catch (err) {
       message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
